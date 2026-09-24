40ac56ecf98d9ff211d5ecf6a9596465c36a9712
Super Render 64 DD <superrender64@gmail.com>
2026-09-24 21:05:22 +1000

    Add files via upload

--- assets/sidebarcomponent.js
+++ assets/sidebarcomponent.js
@@ -1,9 +1,9 @@
 document.getElementById("sidebar").innerHTML = `<rbow class="rbow"><div class="linkbutton sr64ddlink"><b><i>Super Render 64 DD</i></b></div></rbow>
-                                                <a href="index.html"><div class="linkbutton linkbuttona"><b><i>Cruising the Canyon</i></b></div></a>
-                                                <a href="gall.html"><div class="linkbutton linkbuttona"><b><i>Gallery</i></b></div></a>
-                                                <a href="anim.html"><div class="linkbutton linkbuttona"><b><i>Animations</i></b></div></a>
-                                                <a href="stuff.html"><div class="linkbutton linkbuttona"><b><i>Other Stuff</i></b></div></a>
-                                                <a href="about.html"><div class="linkbutton linkbuttona"><b><i>About</i></b></div></a>
-                                                <a href="links.html"><div class="linkbutton linkbuttona"><b><i>Links</i></b></div></a>
-                                                <a href="credit.html"><div class="linkbutton linkbuttona credbutton"><b><i>Credits</i></b></div></a>
-                                                <div class="bottomsidebar"><a class="clipboard" href="tally.html"></a></div>`;
+                <a href="index.html"><div class="linkbutton linkbuttona"><b><i>Cruising the Canyon</i></b></div></a>
+                <a href="gall.html"><div class="linkbutton linkbuttona"><b><i>Gallery</i></b></div></a>
+                <a href="anim.html"><div class="linkbutton linkbuttona"><b><i>Animations</i></b></div></a>
+                <a href="stuff.html"><div class="linkbutton linkbuttona"><b><i>Other Stuff</i></b></div></a>
+                <a href="about.html"><div class="linkbutton linkbuttona"><b><i>About</i></b></div></a>
+                <a href="links.html"><div class="linkbutton linkbuttona"><b><i>Links</i></b></div></a>
+                <a href="credit.html"><div class="linkbutton linkbuttona credbutton"><b><i>Credits</i></b></div></a>
+                <div class="bottomsidebar"><a class="guestbook" href="guestbook.html"></a><a class="clipboard" href="tally.html"></a></div>`;
